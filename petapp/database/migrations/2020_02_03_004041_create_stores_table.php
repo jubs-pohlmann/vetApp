be6c8c507ecd288248b5260448cbc3f6722ecc4b
Mileny Loyolla <mileny.loyolla@gmail.com>
2020-02-02 22:18:10 -0300

    + Tabelas criadas

--- petapp/database/migrations/2020_02_03_004041_create_stores_table.php
+++ petapp/database/migrations/2020_02_03_004041_create_stores_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateStoresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('stores', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('rating');
             $table->string('photo');
             $table->string('phone');
             $table->string('address');
-            $table->string('birthdate');
-            $table->string('cpf');
-            $table->rememberToken();
+            $table->string('email');
+            $table->string('password');
+            $table->string('cnpj');
+            $table->boolean('delivery');
             $table->timestamps();
         });
     }
@@ -36,6 +35,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('stores');
     }
 }
---
+++ petapp/database/migrations/2020_02_03_004103_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('photo');
+            $table->string('name');
+            $table->string('price');
+            $table->string('category');
+            $table->longText('description');
+            $table->string('animal');
+            $table->integer('stock');
+            $table->boolean('status_stock');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}

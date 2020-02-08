842e866bda0e642d4e0d36b9e1798272f142a4ab
bellav0 <isabellaraymundo38@gmail.com>
2020-02-08 20:42:29 -0300

    + toast avisando da solicitação de compra

--- Ionic/src/app/produto/produto.page.ts
+++ Ionic/src/app/produto/produto.page.ts
@@ -11,18 +11,16 @@ export class ProdutoPage implements OnInit {
 
   constructor(public router: Router, public toastController: ToastController) { }
 
-  async presentToast() {
-  	const toast = await this.toastController.create({
-  		message: 'Aguardando confirmação de compra',
-  		duration: 2000
-  	});
+    async presentToast() {
+    	const toast = await this.toastController.create({
+    		message: 'Solicitação de compra enviada para o vendedor. Aguarde a confirmação.',
+    		duration: 3000
+    	});
 
-  	toast.present();
-  }
+    	toast.present();
+    }
 
-  irParaLoja(){
-  	this.router.navigate(['/login'])
-  }
+  
   
   ngOnInit() {
     

60836c104603167c7c872dc12f5e609c9fe8cf68
Júlia Pohlmann <juliapohlmann@hotmail.com>
2020-02-11 16:57:17 -0200

    pre-merge

--- Ionic/src/app/cadastroloja/cadastroloja.page.ts
+++ Ionic/src/app/cadastroloja/cadastroloja.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-cadastroloja',
   templateUrl: './cadastroloja.page.html',
@@ -10,13 +10,18 @@ import { Router } from '@angular/router';
 export class CadastrolojaPage implements OnInit {
 
   registerForm: FormGroup;
+  verificationError: boolean;
 
-  constructor(public formbuilder: FormBuilder) {
+  constructor(public formbuilder: FormBuilder, private _location: Location) {
     this.registerForm = this.formbuilder.group({
       name: [null, [Validators.required, Validators.minLength(2)]],
       email: [null,[Validators.required,Validators.email]],
       cnpj: [null, [Validators.required,Validators.minLength(1)]], //FIX
-      phone: [null, [Validators.required]]
+      phone: [null, [Validators.required]],
+      delivery: [null, [Validators.required]],
+      address: [null,[Validators.required, Validators.minLength(2)]],
+      password: [null,[Validators.required, Validators.minLength(6)]],
+      passwordVerify: [null,[Validators.required,Validators.minLength(6)]]   
     });
    }
 
@@ -24,6 +29,18 @@ export class CadastrolojaPage implements OnInit {
      console.log(form);
      console.log(form.value);
    }
+
+   passwordVerification(form){
+    if(form.value.password != form.value.passwordVerify){
+      this.verificationError = true;
+    } else{
+      this.verificationError = false;
+    }
+  }
+
+  backButton(){
+    this._location.back();
+  }
 
   ngOnInit() {
   }

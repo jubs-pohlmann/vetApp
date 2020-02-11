9b371d70db8d6eb5034fc91208256d93038ef6ca
bellav0 <isabellaraymundo38@gmail.com>
2020-02-11 16:17:15 -0300

    * alteração dos ícones de volta

--- Ionic/src/app/homecategoria/homecategoria.page.ts
+++ Ionic/src/app/homecategoria/homecategoria.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProdutoService } from '../services/produto.service';
 import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular.common'
+import { Location } from '@angular/common'
 //import { Home } from '../home/home.page';
 
 @Component({
